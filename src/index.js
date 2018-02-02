e0422934736a54c1b12fba5ab8efea0037429cda
Christian Sinvry <sinvry.christian@gmail.com>
2018-02-03 00:53:17 +0100

    separation of components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+// Components
+import Connexion from './components/Connexion';
 // CSS
 import './index.css';
 
-class Connection extends React.Component {
-    render() {
-        return(
-            <div className="connexionBox">
-                 <form className="connexion">
-                    <input type="text" placeholder="Pseudo" required/>
-                    <button type="submit">Go</button>
-                </form>
-            </div>
-        )
-    }
-}
 
 render(
-    <Connection />,
+    <Connexion />,
     document.getElementById('root')
 );
