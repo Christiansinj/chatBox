cd065bb09c94f4832780211d248ec87853e20da0
Christian Sinvry <sinvry.christian@gmail.com>
2018-02-03 00:42:20 +0100

    creation of the input component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// Code React.js
+import React from 'react';
+import { render } from 'react-dom';
+// CSS
+import './index.css';
+
+class Connection extends React.Component {
+    render() {
+        return(
+            <div className="connexionBox">
+                 <form className="connexion">
+                    <input type="text" placeholder="Pseudo" required/>
+                    <button type="submit">Go</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+render(
+    <Connection />,
+    document.getElementById('root')
+);
