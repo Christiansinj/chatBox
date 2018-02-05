084ff0473df6c3fa535cb26c2ec2c67a00831003
Christian Sinvry <sinvry.christian@gmail.com>
2018-02-05 23:00:07 +0100

    create and use du selecteur pseudo

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,7 +4,7 @@ class Message extends React.Component {
     render() {
         return(
           <p className="user-message">
-          Pseudo: Mon mmessage !
+          {this.props.pseudo} : Mon super message !
           </p>
         )
     }
