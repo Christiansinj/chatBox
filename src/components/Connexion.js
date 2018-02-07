9d50b863ead208c34d6b67e250cbe9575c530775
Christian Sinvry <sinvry.christian@gmail.com>
2018-02-08 00:07:14 +0100

    create a function for received a pseudo

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -5,14 +5,15 @@ import React from 'react';
 class Connexion extends React.Component {
 
     goToChat = event => {
-        console.log('YO');
+        event.preventDefault();
+        console.log(this.pseudoInput.value);
     };
 
     render() {
         return(
             <div className="connexionBox" onSubmit={(e) => this.goToChat(e)}>
                  <form className="connexion">
-                    <input type="text" placeholder="Pseudo" required/>
+                    <input type="text" placeholder="Pseudo" required ref={input => {this.pseudoInput = input}}/>
                     <button type="submit">Go</button>
                 </form>
             </div>
