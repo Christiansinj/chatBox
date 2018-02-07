180a7bca94e23641ad3e69554dfe651439a0124f
Christian Sinvry <sinvry.christian@gmail.com>
2018-02-07 23:56:32 +0100

    creation d'event sur la connexion

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -3,9 +3,14 @@
 import React from 'react';
 
 class Connexion extends React.Component {
+
+    goToChat = event => {
+        console.log('YO');
+    };
+
     render() {
         return(
-            <div className="connexionBox">
+            <div className="connexionBox" onSubmit={(e) => this.goToChat(e)}>
                  <form className="connexion">
                     <input type="text" placeholder="Pseudo" required/>
                     <button type="submit">Go</button>
